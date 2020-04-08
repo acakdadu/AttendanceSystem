01f1c323c20579b4af2ad83bf192322f24b2332e
Median Prasetya <mnprasetya@gmail.com>
2020-04-08 19:39:35 +0700

    Hallo Ladies

---
+++ app/Http/Controllers/apiController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Api;
+use Illuminate\Http\Request;
+
+class apiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $conn = Api::all();
+        return response()->json($conn);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Api  $api
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Api $api)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Api  $api
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Api $api)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Api  $api
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Api $api)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Api  $api
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Api $api)
+    {
+        //
+    }
+}
